fd81f142854b84dd951a792dc3f5dbe124c21d64
christianbailon <xtian.bailon@gmail.com>
2020-06-04 06:26:54 +0800

    typescript concepts 01

---
+++ index.ts
@@ -0,0 +1,16 @@
+//ANCHOR Sample types
+let example1: boolean = true;
+let example2: number = 35;
+let example3: string = "Hello World";
+
+//ANCHOR Multiple Types
+let example4: boolean | number = 35;
+
+//ANCHOR Implicit vs Explicit typing
+    //Explicit typing
+        let example5: number;
+        example5 = 35;
+        //const arrayExample number[] = [1, 3, 4, 5];
+    //Implicit Typing
+        //const arrayExample = [1, 3, 4, 5];
+
